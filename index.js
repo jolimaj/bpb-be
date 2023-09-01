690b101e8bb982ddc03ff9b6c4375a0f17c8e880
byaheCSTC <byaheCSTC@gmail.com>
2023-09-01 11:51:32 +0800

    added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.MAIN_URL }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

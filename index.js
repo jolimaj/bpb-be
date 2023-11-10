b34f21b79536ca0a4b1028d5694559d53b2ea634
byaheCSTC <byaheCSTC@gmail.com>
2023-11-11 00:23:01 +0800

    dsf

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      httpOnly: true,
+      // httpOnly: true,
       secure: true,
       sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,

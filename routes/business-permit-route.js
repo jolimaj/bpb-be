f627d84219ce580320c302af40204cd10d8e58d0
byaheCSTC <byaheCSTC@gmail.com>
2023-11-28 13:54:02 +0800

    fddf

--- routes/business-permit-route.js
+++ routes/business-permit-route.js
@@ -436,6 +436,10 @@ router.put(
   async (req, res) => {
     try {
       const { params, body, files, session } = req;
+      console.log(
+        "🚀 ~ file: business-permit-route.js:439 ~ session:",
+        session
+      );
       if (session?.email && session?.password) {
         let data;
         if (body?.release) {
